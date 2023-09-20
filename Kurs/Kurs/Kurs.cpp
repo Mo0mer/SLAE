f90b90dd43a278d1815e212f93e585afdf5c05cf
Mo0mer <104166536+Mo0mer@users.noreply.github.com>
2023-09-20 22:27:47 +0300

    Вариант 4 (Законченный матричный)

--- Kurs/Kurs/Kurs.cpp
+++ Kurs/Kurs/Kurs.cpp
@@ -1,16 +1,24 @@
 ﻿#include <iostream>
 #include <vector>
+#include <cmath>
+#include <iomanip>
 
 class Data {
 private:
     int row; // Отвечает за количество строк
 
 public:
-    Data(int row = 2) : row(row) {}
+    Data(int row = 2) : row(row), answer(row, 0.0) {}
 
-    ~Data() {}
+    ~Data() {
+        // Инициализируем вектор data нулями
+        data.resize(row, std::vector<int>(row, 0));
+        // Инициализируем вектор data_temp нулями
+        data_temp.resize(row, 0);
+    }
 
     std::vector<std::vector<int>> data;
+    std::vector<int> data_temp;
     std::vector<double> answer;
 
     void make_vector() {
@@ -30,7 +38,7 @@ public:
 
         std::cout << "Введите вектор свободных членов: ";
 
-        std::vector<int> data_temp;
+        
         data_temp.reserve(row);
 
         for (int i = 0; i < row; i++) {
@@ -56,11 +64,6 @@ public:
                 data_temp.push_back(del);
             }
         }
-        else {
-            for (int i = 0; i < row; i++) {
-                data[i].push_back(data_temp[i]);
-            }
-        }
     }
 
     void print_vector() {
@@ -70,6 +73,14 @@ public:
             }
             std::cout << '\n';
         }
+
+        std::cout << '\n';
+
+        for (int j = 0; j < row; j++){
+            std::cout << data_temp[j] << ' ';
+        }
+
+        std:: cout << '\n';
     }
 };
 
@@ -84,7 +95,15 @@ public:
 
     ~Kramer() {}
 
+    void make_date() {
+        for (int k = 0; k < data_temp.size(); k++) {
+            data[k].push_back(data_temp[k]);
+        }
+    }
+
     void decision_order_2() {
+        make_date();
+
         det_m = data[0][0] * data[1][1] - data[1][0] * data[0][1];
 
         det_1 = data[0][2] * data[1][1] - data[1][2] * data[0][1];
@@ -111,6 +130,8 @@ public:
     }
 
     void decision_order_3() {
+        make_date();
+
         det_m = data[0][0] * data[1][1] * data[2][2] + data[1][0] * data[2][1] * data[0][2] + data[0][1] * data[1][2] * data[2][0] -
             data[2][0] * data[1][1] * data[0][2] - data[2][1] * data[1][2] * data[0][0] - data[1][0] * data[0][1] * data[2][2];
 
@@ -226,6 +247,122 @@ public:
     Matr(int row = 2) : Data(row) {};
 
     ~Matr() {}
+
+    void print_answer() {
+        std::cout << '\n';
+
+        for (double val : answer) {
+            std::cout << val << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    void multiplyMatrixVector(std::vector<std::vector<double>>& y) {
+        int numRows = y.size();
+        int numCols = y[0].size();
+        int vectorSize = data_temp.size();
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                answer[i] += y[i][j] * data_temp[j];
+            }
+        }
+    }
+    
+    int Gauss(std::vector<std::vector<int>>& matrica_a, int n, std::vector<double>& massiv_b, std::vector<double>& x) {
+        int i, j, k, r;
+        double c, M, max, s;
+        std::vector<std::vector<double>> a(n, std::vector<double>(n));
+        std::vector<double> b(n);
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                a[i][j] = matrica_a[i][j];
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+            b[i] = massiv_b[i];
+        }
+
+        for (k = 0; k < n; k++) {
+            max = fabs(a[k][k]);
+            r = k;
+            for (i = k + 1; i < n; i++) {
+                if (fabs(a[i][k]) > max) {
+                    max = fabs(a[i][k]);
+                    r = i;
+                }
+            }
+            for (j = 0; j < n; j++) {
+                c = a[k][j];
+                a[k][j] = a[r][j];
+                a[r][j] = c;
+            }
+            c = b[k];
+            b[k] = b[r];
+            b[r] = c;
+            for (i = k + 1; i < n; i++) {
+                for (M = a[i][k] / a[k][k], j = k; j < n; j++) {
+                    a[i][j] -= M * a[k][j];
+                }
+                b[i] -= M * b[k];
+            }
+        }
+
+        if (a[n - 1][n - 1] == 0) {
+            if (b[n - 1] == 0) return -1;
+            else return -2;
+        }
+        else {
+            for (i = n - 1; i >= 0; i--) {
+                for (s = 0, j = i + 1; j < n; j++) {
+                    s += a[i][j] * x[j];
+                }
+                x[i] = (b[i] - s) / a[i][i];
+            }
+            return 0;
+        }
+    }
+
+    int INVERSE(std::vector<std::vector<int>>& a, int n, std::vector<std::vector<double>>& y) {
+        int i, j, res = 0;
+        std::vector<double> b(n);
+        std::vector<double> x(n);
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (j == i) b[j] = 1;
+                else b[j] = 0;
+            }
+            res = Gauss(a, n, b, x);
+            if (res != 0) break;
+            else {
+                for (j = 0; j < n; j++) {
+                    y[j][i] = x[j];
+                }
+            }
+        }
+
+        if (res != 0) return -1;
+        else return 0;
+    }
+
+    void begin_inverse() {
+        int N = data_temp.size();
+
+        std::vector<std::vector<double>> matr_one(N, std::vector<double>(N));
+        
+        int result = INVERSE(data, N, matr_one);
+
+        if (result == 0) {
+            multiplyMatrixVector(matr_one);
+        }
+        else {
+            std::cout << "Обратной матрицы не существует, СЛАУ решений не имеет" << std::endl;
+        }
+    }
+
 };
 
 int main() {
@@ -239,7 +376,9 @@ int main() {
 
     int method;
 
-    std::cout << "Выберите метод решения СЛАУ:" << '\n' << "1) Крамер" << '\n' << "2) Гаус" << std::endl << '\n';
+    std::cout << "Выберите метод решения СЛАУ:" << '\n' << "1) Крамер" << '\n' << 
+                                                            "2) Гаус" << std::endl <<
+                                                            "3) Матричный" << std::endl << '\n';
     std::cin >> method;
     std::cout << '\n';
 
@@ -261,6 +400,12 @@ int main() {
         gs.make_vector();
         gs.SolveGaussian(r);
         gs.print_answer();
+    }
+    else if (method == 3) {
+        Matr mt(r);
+        mt.make_vector();
+        mt.begin_inverse();
+        mt.print_answer();
     }
     else {
         std::cout << "Неверный метод. Выход." << std::endl;
